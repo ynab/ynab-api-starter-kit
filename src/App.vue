b5879ba63d5383a3b0edae4456c3472c163263a3
Brady Holt <brady.holt@gmail.com>
2018-07-11 12:46:40 -0500

    Move Privacy Policy to footer

--- src/App.vue
+++ src/App.vue
@@ -44,10 +44,6 @@
           <button class="btn btn-info" @click="budgetId = null">&lt; Select Another Budget</button>
         </div>
 
-        <div>
-          <h2>Privacy Policy</h2>
-          <p>This website does not store any information from you or your YNAB account. All data retrieved from the YNAB API is stored only in your browser and is never transmitted to any other location or third-party.</p>
-        </div>
       </div>
 
       <Footer />
