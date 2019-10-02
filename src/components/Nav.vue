b929d2f6c66c45baa3d6261827dc340413842022
David Angel <david@davidangel.net>
2019-10-01 21:10:10 -0400

    Improve the Bootstrap Template

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-    <h5 class="my-0 mr-md-auto font-weight-normal">
-      App Name
-    </h5>
+  <div
+    class="navbar navbar-dark bg-primary d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow"
+  >
+    <div class="container">
+      <h5 class="my-0 mr-md-auto font-weight-normal navbar-brand">YNAB API Starter Kit</h5>
+    </div>
   </div>
 </template>
