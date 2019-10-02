ed719f64f5fb3ed462bc2a7be290d0fccc80b37b
Kyle Robinson Young <kyle@dontkry.com>
2019-10-02 09:30:14 -0700

    formatting nit pick on template

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="navbar navbar-dark bg-primary d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow"
-  >
+  <div class="navbar navbar-dark bg-primary d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow">
     <div class="container">
       <h5 class="my-0 mr-md-auto font-weight-normal navbar-brand">YNAB API Starter Kit</h5>
     </div>
