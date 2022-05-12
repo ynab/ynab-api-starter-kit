9131fee94e8d84023e1ff8e3dfc76ece4192e150
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-05-11 21:02:38 -0400

    Cleaned up Webpack deploy! (locally especially)

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-  entry: './src/main.js',
-  mode: process.env.NODE_ENV || 'development',
+  entry: "./src/main.js",
+  mode: process.env.NODE_ENV || "development",
   output: {
-    filename: 'build.js',
-    publicPath: '/dist/',
-    path: path.resolve(__dirname, './dist')
+    filename: "build.js",
+    publicPath: "/dist/",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./public"),
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: ['vue-loader']
-      }
-    ]
+        use: ["vue-loader"],
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
-    new VueLoaderPlugin()
-  ]
+    new VueLoaderPlugin(),
+  ],
 };
