edc4a5b33ac27dc5623e035292f2e808b9fb854f
Brady Holt <brady.holt@gmail.com>
2023-03-20 11:21:39 -0600

    Update some dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require("path");
-const webpack = require("webpack");
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   entry: "./src/main.js",
-  mode: process.env.NODE_ENV || "development",
   output: {
     filename: "build.js",
     publicPath: "/dist/",
@@ -22,11 +20,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
     new VueLoaderPlugin(),
   ],
 };
